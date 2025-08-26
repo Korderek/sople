8d01c14dc47fc6a393e60b7330cfeb206992bde0
Karol Bąk <bak.karolek@gmail.com>
2025-08-26 23:22:03 +0200

    Dodano nowe zadania w README.md, dodano możliwość zmiany animacji w player.lua

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 ### Zadania
 
+### Zadania
+
 - [x] Gdy mamy 5 pieniążków zmień skina gracza na "śpioszka"
 - [x] Gdy gracz jest śpioszkiem... (uzupełnij) (ma ciemniej przed ekranem)
 - [x] Gdy gracz jest śpioszkiem, napisy mają być białe
@@ -11,10 +13,19 @@
 - [x] spadajacy sklep
 - [x] wyswietl sklepik gdy zebrany spadajacy sklep
 - [x] kilka opcji w sklepiku - szybkość, życia, lepszy skin
-- [x] rzeczy w sklepie kosztuja monety i jest cena pokazana 
-- [x] (trudne) na czas wizyty w sklepie gra sie zatrzymuje 
-- [ ] Gdy gracz jest śpioszkiem, sople lecą 10% wolniej
-- [ ] Dodaj lepsze dialogi 
-- [ ] kolejne przednioty w sklepiku 
-- [ ] światy do kupienia w sklepie 
-- [ ] ekran światów
+- [x] rzeczy w sklepie kosztuja monety i jest cena pokazana
+- [x] (trudne) na czas wizyty w sklepie gra sie zatrzymuje
+- [ ] Śpioszek - Gdy gracz jest śpioszkiem, sople lecą 10% wolniej
+- [ ] Dodaj nowe przedmioty w sklepiku:
+  - [ ] Sklepik - nowy przedmiot - puste serce - zwiększa limit serc o 1
+  - [ ] Sklepik - nowy przedmiot - wślizg - odblokowuje umiejętność wślizgu - zwiększenie prędkości na 0.3s do 10 po kliknięciu "s" na klawiaturze
+- [ ] Po otwarciu sklepu wylosuj 3 towary, które będą możliwe do kupienia
+- [ ] Wyświetl grafiki planet na ekranie wyboru świata:
+  - [ ] Planety na ekranie światów - Dzika knieja - grafika z lasem
+  - [ ] Planety na ekranie światów - Pustynia zatracenia - grafika z pustynią
+- [ ] Zmieniaj animację gracza w zależności od sytuacji:
+  - [ ] Gdy niesmiertelny > 0, wyświetl grafikę 'zraniony'
+  - [ ] Gdy zbierze monetę, wyświetl przez 0.3s grafikę 'radosny'
+  - [ ] Gdy stoi, wyświetl grafikę 'stoi'
+  - [ ] Gdy idzie, wyświetl grafikę 'idzie'
+
--- src/player.lua
+++ src/player.lua
@@ -1,14 +1,14 @@
 local Player = {}
 
-local tekstura = love.graphics.newImage("gfx/gracz-animacje.png")
+local tekstura_standard = love.graphics.newImage("gfx/gracz-animacje.png")
 local tekstura_spioszek = love.graphics.newImage("gfx/spioszek-animacje.png")
-local animacja = { -- wymiary i położenie klatek animacji na teksturze
+local animacje = { -- wymiary i położenie klatek animacji na teksturze
     standard = {
-        stoi = love.graphics.newQuad(18, 18, 109, 235, tekstura),
-        zraniony = love.graphics.newQuad(131, 18, 113, 235, tekstura),
-        idzie = love.graphics.newQuad(244, 18, 122, 235, tekstura),
-        wslizg = love.graphics.newQuad(361, 18, 164, 235, tekstura),
-        radosny = love.graphics.newQuad(521, 18, 150, 235, tekstura),
+        stoi = love.graphics.newQuad(18, 18, 109, 235, tekstura_standard),
+        zraniony = love.graphics.newQuad(131, 18, 113, 235, tekstura_standard),
+        idzie = love.graphics.newQuad(244, 18, 122, 235, tekstura_standard),
+        wslizg = love.graphics.newQuad(361, 18, 164, 235, tekstura_standard),
+        radosny = love.graphics.newQuad(521, 18, 150, 235, tekstura_standard),
     },
     spioszek = {
         stoi = love.graphics.newQuad(15, 10, 134, 235, tekstura_spioszek),
@@ -23,8 +23,15 @@ function Player.draw()
     local scale_x = gracz.kierunek == "prawo" and gracz.scale or -gracz.scale
     local scale_y = gracz.scale
 
+    local tekstura = tekstura_standard
+    local animacja = animacje.standard
+    if gracz.skin == spioszekImg then
+        tekstura = tekstura_spioszek
+        animacja = animacje.spioszek
+    end
+
     love.graphics.setColor(1, 1, 1)
-    love.graphics.drawCentered(gracz.skin, gracz.x, gracz.y, gracz.width, gracz.height, scale_x, scale_y)
+    love.graphics.drawQuadCentered(tekstura, animacja.stoi, gracz.x, gracz.y, gracz.width, gracz.height, scale_x, scale_y)
     love.graphics.rectangleDebug(gracz.x, gracz.y, gracz.width, gracz.height)
 end
 
