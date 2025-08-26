376b6fdf2ce8a2f7844ab0ee53f1beb1de8f3ecd
Karol Bąk <bak.karolek@gmail.com>
2025-08-26 08:11:32 +0200

    Wersja 0.14.1 Dodanie pliku animacji dla śpioszka

--- src/player.lua
+++ src/player.lua
@@ -1,12 +1,22 @@
 local Player = {}
 
 local tekstura = love.graphics.newImage("gfx/gracz-animacje.png")
+local tekstura_spioszek = love.graphics.newImage("gfx/spioszek-animacje.png")
 local animacja = { -- wymiary i położenie klatek animacji na teksturze
-    stoi = love.graphics.newQuad(18, 18, 109, 235, tekstura),
-    zraniony = love.graphics.newQuad(131, 18, 113, 235, tekstura),
-    idzie = love.graphics.newQuad(244, 18, 122, 235, tekstura),
-    wslizg = love.graphics.newQuad(361, 18, 164, 235, tekstura),
-    radosny = love.graphics.newQuad(521, 18, 150, 235, tekstura),
+    standard = {
+        stoi = love.graphics.newQuad(18, 18, 109, 235, tekstura),
+        zraniony = love.graphics.newQuad(131, 18, 113, 235, tekstura),
+        idzie = love.graphics.newQuad(244, 18, 122, 235, tekstura),
+        wslizg = love.graphics.newQuad(361, 18, 164, 235, tekstura),
+        radosny = love.graphics.newQuad(521, 18, 150, 235, tekstura),
+    },
+    spioszek = {
+        stoi = love.graphics.newQuad(15, 10, 134, 235, tekstura_spioszek),
+        zraniony = love.graphics.newQuad(147, 10, 122, 235, tekstura_spioszek),
+        idzie = love.graphics.newQuad(278, 10, 129, 235, tekstura_spioszek),
+        wslizg = love.graphics.newQuad(407, 10, 133, 235, tekstura_spioszek),
+        radosny = love.graphics.newQuad(547, 10, 145, 235, tekstura_spioszek),
+    }
 }
 
 function Player.draw()
