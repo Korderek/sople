8df70b071dd733f580e61fc3dc7a2d8524208f08
Korderek <kordekyt@outlook.com>
2025-09-26 16:17:56 +0200

    Wersja 0.16

--- planety/pustynia.lua
+++ planety/pustynia.lua
@@ -1,16 +1,40 @@
 local Pustynia = {}
 local Player = require("src.player")
-
+local listakaktusow = {}
+local kaktusImg = love.graphics.newImage("gfx/gracz.png")
 Pustynia.load = function()
+    kaktus = {
+        x = szerokosc + 100,
+        y = 800,
+    }
+
     gracz.y = 800
     gracz.x = 300
 end
+
 Pustynia.update = function(dt)
-    if love.keyboard.isDown("w") then
-        gracz.y = gracz.y + 100
+    --gracz.y = gracz.y + 5
+    kaktus.x = kaktus.x - 10
+    if kaktus.x < -100 then
+        kaktus.x = szerokosc + love.math.random(200, 800)
+        table.insert(listakaktusow,
+            { x = kaktus.x, y = kaktus.y, width = kaktusImg:getWidth(), height = kaktusImg:getHeight() })
+    end
+    local przyspieszenie = 0
+    if love.keyboard.isDown("w") and gracz.y == 800 then
+        przyspieszenie = -37
+    end
+    przyspieszenie = przyspieszenie + 1.56
+    gracz.predkosc = gracz.predkosc + przyspieszenie
+    gracz.y = gracz.y + gracz.predkosc
+    if gracz.y > 800 then
+        gracz.y = 800
+        gracz.predkosc = 0
     end
 end
+
 Pustynia.draw = function()
+    love.graphics.drawCentered(kaktusImg, kaktus.x, kaktus.y, 1, 1)
     Player.draw()
     love.graphics.setBackgroundColor(0.9, 0.8, 0.5)
 end
